656284f4bc6de32aba5bc640d70827458380b292
Alex <alext@rtradetechnologies.com>
2019-05-28 14:04:34 -0700

    rtns: add mocks for tests

--- rtns/host_test.go
+++ rtns/host_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	cfg "github.com/RTradeLtd/config/v2"
+	pb "github.com/RTradeLtd/grpc/krab"
 	kaas "github.com/RTradeLtd/kaas/v2"
 	lp "github.com/RTradeLtd/rtns/internal/libp2p"
 	"github.com/RTradeLtd/rtns/mocks"
@@ -27,20 +28,52 @@ func Test_New_Publisher(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	publisher := newPublisher(ctx, t)
+	//////////////////
+	// setup mocks //
+	////////////////
+
+	pk1 := newPK(t)
+	pk1Bytes, err := pk1.Bytes()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pk2 := newPK(t)
+	pk2Bytes, err := pk2.Bytes()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fkb := &mocks.FakeServiceClient{}
+	fkb.GetPrivateKeyReturnsOnCall(0, &pb.Response{Status: "Ok", PrivateKey: pk1Bytes}, nil)
+	fkb.GetPrivateKeyReturnsOnCall(1, &pb.Response{Status: "Ok", PrivateKey: pk2Bytes}, nil)
+
+	fns := &mocks.FakeNameSystem{}
+	fns.PublishReturnsOnCall(0, nil)
+	fns.PublishReturnsOnCall(1, nil)
+	fns.PublishReturnsOnCall(2, nil)
+	fns.PublishReturnsOnCall(3, nil)
+
+	//////////////////////
+	// setup publisher //
+	////////////////////
+
+	publisher := newPublisher(ctx, t, fkb, fns)
 	defer publisher.Close()
 	publisher.Bootstrap(lp.DefaultBootstrapPeers())
 
+	//////////////////
+	// start tests //
+	////////////////
+
 	// ensure no previous records have been published
-	if err := publisher.republishEntries(); err == nil {
-		t.Fatal("error expected")
+	if err := publisher.republishEntries(); err != errNoRecordsPublisher {
+		t.Fatal("wrong error received")
 	}
 
-	pk1 := newPK(t)
 	if err := publisher.Publish(ctx, pk1, "pk1", ipfsPath1); err != nil {
 		t.Fatal(err)
 	}
 	if len(publisher.cache.List()) != 1 {
+		fmt.Println("cache length:", len(publisher.cache.List()))
 		t.Fatal("invalid cache length")
 	}
 	pid, err := peer.IDFromPublicKey(pk1.GetPublic())
@@ -49,11 +82,11 @@ func Test_New_Publisher(t *testing.T) {
 	}
 	fmt.Println("pk1", pid.String())
 
-	pk2 := newPK(t)
-	if err := publisher.Publish(ctx, pk2, "pk1", ipfsPath2); err != nil {
+	if err := publisher.Publish(ctx, pk2, "pk2", ipfsPath2); err != nil {
 		t.Fatal(err)
 	}
 	if len(publisher.cache.List()) != 2 {
+		fmt.Println("cache length:", len(publisher.cache.List()))
 		t.Fatal("invalid cache length")
 	}
 	pid, err = peer.IDFromPublicKey(pk2.GetPublic())
@@ -62,9 +95,12 @@ func Test_New_Publisher(t *testing.T) {
 	}
 	fmt.Println("pk2", pid.String())
 
+	if err := publisher.republishEntries(); err != nil {
+		t.Fatal(err)
+	}
 }
 
-func newPublisher(ctx context.Context, t *testing.T) *Publisher {
+func newPublisher(ctx context.Context, t *testing.T, fkb *mocks.FakeServiceClient, fns *mocks.FakeNameSystem) *Publisher {
 	pk := newPK(t)
 	addr, err := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/4005")
 	if err != nil {
@@ -74,7 +110,8 @@ func newPublisher(ctx context.Context, t *testing.T) *Publisher {
 	if err != nil {
 		t.Fatal(err)
 	}
-	publisher.keys.kb = &kaas.Client{ServiceClient: &mocks.FakeServiceClient{}}
+	publisher.keys.kb = &kaas.Client{ServiceClient: fkb}
+	publisher.ns = fns
 	return publisher
 }
 func newPK(t *testing.T) crypto.PrivKey {
