7fedc1fa12f99eeadd0c9952b0e75bf797015a45
Alex <alext@rtradetechnologies.com>
2019-06-01 23:28:40 -0700

    tns: initial commit

---
+++ tns/doc.go
@@ -0,0 +1,11 @@
+// Package tns is used for federated IPFS name resolution layer
+// combining BNS (Better Name Service) to achieve DNS link resolution performance
+package tns
+
+// Server is the TNS equivalent of a DNS server
+type Server struct {
+}
+
+// Client is the TNS equivalent of a DNS client
+type Client struct {
+}
