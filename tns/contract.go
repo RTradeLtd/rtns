ddd01f20f198422eeebf57e133895a1187ce0558
Alex <alext@rtradetechnologies.com>
2019-06-01 23:36:54 -0700

    tns: add contract addresses

---
+++ tns/contract.go
@@ -0,0 +1,8 @@
+package tns
+
+const (
+	// MainnetContractAddress is the address of the mainnet BNS contract
+	MainnetContractAddress = "0x15dA427d0c2119d2B96CECB0CE1884f0077B24eA"
+	// RopstenContractAddress is th eaddress of the ropsten BNS contract
+	RopstenContractAddress = "0xbc192f3a81bca5b057928e16a6ade1c7abc67077"
+)
