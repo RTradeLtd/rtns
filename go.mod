4d4b77ac119298672518ceabae7fc13d0637c55a
Alex <alext@rtradetechnologies.com>
2019-07-04 14:28:11 -0700

    chore: initial abstraction of setup

--- go.mod
+++ go.mod
@@ -3,34 +3,40 @@ module github.com/RTradeLtd/rtns
 go 1.12
 
 require (
-	github.com/RTradeLtd/config v2.0.5+incompatible // indirect
-	github.com/RTradeLtd/config/v2 v2.1.4 // indirect
+	github.com/RTradeLtd/go-libp2p-testutils v1.0.0
 	github.com/RTradeLtd/grpc v0.0.0-20190528193535-5184ecc77228
-	github.com/RTradeLtd/kaas/v2 v2.1.2
-	github.com/RTradeLtd/rtfs/v2 v2.1.2 // indirect
+	github.com/RTradeLtd/kaas/v2 v2.1.3
+	github.com/btcsuite/btcd v0.0.0-20190605094302-a0d1e3e36d50 // indirect
 	github.com/davidlazar/go-crypto v0.0.0-20190522120613-62389b5e4ae0 // indirect
 	github.com/dgryski/go-farm v0.0.0-20190423205320-6a90982ecee2 // indirect
 	github.com/gogo/protobuf v1.2.1
 	github.com/google/go-cmp v0.3.0 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20190430165422-3e4dfb77656c // indirect
+	github.com/ipfs/go-bitswap v0.1.1 // indirect
 	github.com/ipfs/go-datastore v0.0.5
+	github.com/ipfs/go-ds-badger v0.0.4 // indirect
 	github.com/ipfs/go-ipfs v0.4.21
 	github.com/ipfs/go-ipfs-config v0.0.6
 	github.com/ipfs/go-ipfs-keystore v0.0.1
 	github.com/ipfs/go-ipns v0.0.1
+	github.com/ipfs/go-merkledag v0.1.0 // indirect
 	github.com/ipfs/go-path v0.0.7
 	github.com/ipfs/go-unixfs v0.0.8 // indirect
-	github.com/ipfs/interface-go-ipfs-core v0.0.9
-	github.com/libp2p/go-libp2p v0.1.1
+	github.com/ipfs/interface-go-ipfs-core v0.1.0
+	github.com/libp2p/go-libp2p v0.2.0
 	github.com/libp2p/go-libp2p-circuit v0.1.0
-	github.com/libp2p/go-libp2p-core v0.0.3
+	github.com/libp2p/go-libp2p-core v0.0.6
 	github.com/libp2p/go-libp2p-crypto v0.1.0
-	github.com/libp2p/go-libp2p-host v0.1.0 // indirect
-	github.com/libp2p/go-libp2p-kad-dht v0.1.0
-	github.com/libp2p/go-libp2p-peerstore v0.1.0
+	github.com/libp2p/go-libp2p-kad-dht v0.1.1
+	github.com/libp2p/go-libp2p-peerstore v0.1.1
 	github.com/libp2p/go-libp2p-pnet v0.1.0
 	github.com/libp2p/go-libp2p-record v0.1.0
+	github.com/libp2p/go-libp2p-transport v0.0.5 // indirect
 	github.com/multiformats/go-multiaddr v0.0.4
-	google.golang.org/genproto v0.0.0-20190508193815-b515fa19cec8 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
+	google.golang.org/genproto v0.0.0-20190502173448-54afdca5d873 // indirect
 	google.golang.org/grpc v1.20.1
 )
+
+replace github.com/dgraph-io/badger v2.0.0-rc.2+incompatible => github.com/dgraph-io/badger/v2 v2.0.0-rc.2
--- host.go
+++ host.go
@@ -2,111 +2,41 @@ package rtns
 
 import (
 	"context"
-	"fmt"
 	"time"
 
-	kaas "github.com/RTradeLtd/kaas/v2"
-	lp "github.com/RTradeLtd/rtns/internal/libp2p"
 	"github.com/ipfs/go-datastore"
+	keystore "github.com/ipfs/go-ipfs-keystore"
 	"github.com/ipfs/go-ipfs/namesys"
 	"github.com/ipfs/go-path"
-	pinfo "github.com/libp2p/go-libp2p-core"
 	ci "github.com/libp2p/go-libp2p-core/crypto"
-	host "github.com/libp2p/go-libp2p-core/host"
-	peerstore "github.com/libp2p/go-libp2p-core/peerstore"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	"github.com/libp2p/go-libp2p-peerstore/pstoremem"
-	"github.com/multiformats/go-multiaddr"
 )
 
-// Service implements the rtns logic
-// as a consumable service or library.
-type Service interface {
-	// Close is used to trigger a shutdown of internal services
-	Close()
-
-	// DefaultBootstrapPeers returns the normal libp2p bootstrap peers, as well as the production nodes of Temporal.
-	DefaultBootstrapPeers() []pinfo.PeerAddrInfo
-	// Bootstrap is used to bootstrap the dht
-	Bootstrap(peers []pinfo.PeerAddrInfo)
-
-	// Publish enables publishing of an IPNS record with a default lifetime of 24 hours
-	Publish(ctx context.Context, pk ci.PrivKey, cache bool, keyID, content string) error
-	// PublishWithEOL allows specifying a lifetime for this record overriding the default lifetime of 24 hours
-	PublishWithEOL(ctx context.Context, pk ci.PrivKey, eol time.Time, cache bool, keyID, content string) error
-
-	// GetKey returns a key from the underlying krab keystore
-	GetKey(name string) (ci.PrivKey, error)
-	// HasKey returns whether or not the key is in our keystore
-	HasKey(name string) (bool, error)
-}
-
-// Config is used to configure the RTNS service
-type Config struct {
-	Datastore   datastore.Batching
-	PK          ci.PrivKey
-	ListenAddrs []multiaddr.Multiaddr
-	Secret      []byte
-}
-
-// rtns manages all the needed components
+// RTNS manages all the needed components
 // to interact with public and private IPNS
 // networks.
-type rtns struct {
-	h     host.Host
-	pk    ci.PrivKey
-	d     *dht.IpfsDHT
-	ds    datastore.Datastore
+type RTNS struct {
 	ns    namesys.NameSystem
-	ps    peerstore.Peerstore
+	ds    datastore.Datastore
 	ctx   context.Context
-	keys  *rkeystore
+	keys  keystore.Keystore
 	cache *cache
 }
 
-// NewService is used to instantiate an RTNS publisher service
-func NewService(ctx context.Context, kbClient *kaas.Client, cfg Config) (Service, error) {
-	return newRTNS(ctx, kbClient, cfg)
-}
-
-// NewRTNS is used to instantiate our RTNS service, and start the republisher
-// intended to be used as a `Publisher` type by external libraries
-func newRTNS(ctx context.Context, kbClient *kaas.Client, cfg Config) (*rtns, error) {
-	ps := pstoremem.NewPeerstore()
-	ht, dt, err := lp.SetupLibp2p(ctx, cfg.PK, cfg.Secret, cfg.ListenAddrs, ps, cfg.Datastore)
-	if err != nil {
-		return nil, err
-	}
-	r := &rtns{
-		h:     ht,
-		d:     dt,
-		pk:    cfg.PK,
-		ds:    cfg.Datastore,
-		ps:    ps,
-		ns:    namesys.NewNameSystem(dt, cfg.Datastore, 128),
-		ctx:   ctx,
-		keys:  newRKeystore(ctx, kbClient),
+// NewRTNS instantiates our RTNS service, and starts the republisher
+func NewRTNS(ctx context.Context, dt *dht.IpfsDHT, ds datastore.Datastore, keys keystore.Keystore, size int) *RTNS {
+	r := &RTNS{
+		ns:    namesys.NewNameSystem(dt, ds, size),
+		ds:    ds,
+		keys:  keys,
 		cache: newCache(),
 	}
 	go r.startRepublisher()
-	return r, nil
-}
-
-// Close is used to close all service needed by our publisher
-func (r *rtns) Close() {
-	if err := r.d.Close(); err != nil {
-		fmt.Println("error shutting down dht:", err.Error())
-	}
-	if err := r.h.Close(); err != nil {
-		fmt.Println("error shutting down host:", err.Error())
-	}
-	if err := r.ds.Close(); err != nil {
-		fmt.Println("error shutting down datastore:", err.Error())
-	}
+	return r
 }
 
 // Publish enables publishing of an IPNS record with a default lifetime of 24 hours
-func (r *rtns) Publish(ctx context.Context, pk ci.PrivKey, cache bool, keyID, content string) error {
+func (r *RTNS) Publish(ctx context.Context, pk ci.PrivKey, cache bool, keyID, content string) error {
 	if cache {
 		r.cache.Set(keyID)
 	}
@@ -114,7 +44,7 @@ func (r *rtns) Publish(ctx context.Context, pk ci.PrivKey, cache bool, keyID, co
 }
 
 // PublishWithEOL allows specifying a lifetime for this record overriding the default lifetime of 24 hours
-func (r *rtns) PublishWithEOL(ctx context.Context, pk ci.PrivKey, eol time.Time, cache bool, keyID, content string) error {
+func (r *RTNS) PublishWithEOL(ctx context.Context, pk ci.PrivKey, eol time.Time, cache bool, keyID, content string) error {
 	if cache {
 		r.cache.Set(keyID)
 	}
@@ -122,12 +52,11 @@ func (r *rtns) PublishWithEOL(ctx context.Context, pk ci.PrivKey, eol time.Time,
 }
 
 // GetKey returns a key from the underlying krab keystore
-
-func (r *rtns) GetKey(name string) (ci.PrivKey, error) {
+func (r *RTNS) GetKey(name string) (ci.PrivKey, error) {
 	return r.keys.Get(name)
 }
 
 // HasKey returns whether or not the key is in our keystore
-func (r *rtns) HasKey(name string) (bool, error) {
+func (r *RTNS) HasKey(name string) (bool, error) {
 	return r.keys.Has(name)
 }
